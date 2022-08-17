016672a68178ad2ace487f6216426bf7637022e4
Luiz Sampaio <lsampaio@miu.edu>
2022-08-16 22:49:47 -0500

    tested hasBingo

--- js/interviewCodingChallenge-master/index.js
+++ js/interviewCodingChallenge-master/index.js
@@ -110,3 +110,40 @@ const hasBingo = function (drawnNumbers, elements){
     return true;
 };
 
+
+
+//module.exports = getLine();
+
+// here are some samples
+
+// this should return true with diagonal + free
+const bingoCard = [
+    8, 29, 35, 54, 65,
+    13, 24, 44, 48, 67,
+    9, 21, 'free', 59, 63,
+    7, 19, 34, 53, 61,
+    1, 20, 33, 46, 72
+];
+const drawnNumbers = [1, 33, 53,65, 46];
+const array1 =[1, 33, 53, 65, 46];
+console.log(bingoCard);
+console.log(hasBingo(drawnNumbers,array1));
+
+
+//const diagonal =getRightDiagonal(bingoCard);
+
+// this should return false
+// checkForBingo(
+//   [
+//    8, 29, 35, 54, 65,
+//    13, 24, 44, 48, 67,
+//    9, 21, 'FREE', 59, 63,
+//    7, 19, 34, 53, 61,
+//    1, 20, 33, 46, 72
+//   ],
+//   [
+//     1, 33, 53, 65, 29, 75
+//   ]
+// );
+
+
